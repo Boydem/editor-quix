94a6151fa2e06255f9d32f67de59bf4015ae0d57
ForeseN <yagosik@gmail.com>
2023-01-17 17:37:46 +0200

    moved routes to index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './root-cmp'
 import { store } from './js/store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <Provider store={store}>
-        {/* <Router> */}
+        <Router>
         <App />
-        {/* </Router> */}
+        </Router>
     </Provider>
 )
--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,6 +1,6 @@
 import './assets/styles/styles.scss'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { AppHeader } from './js/pages/Home/cmps/home-header'
 import { Home } from './js/pages/Home/home'
 import { TemplateIndex } from './js/pages/Template/template-index'
@@ -11,13 +11,10 @@ function App() {
         <div className='app'>
             <AppHeader />
             <main className=' main-layout'>
-                <Router>
-                    {/* <AppHeader /> */}
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/create' element={<TemplateIndex />} />
-                    </Routes>
-                </Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/create' element={<TemplateIndex />} />
+                </Routes>
             </main>
             <AppFooter />
         </div>
