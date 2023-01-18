d495482a7a2aafea6e117c0f3730a9462a314d6f
Boydem <noyemdahan@gmail.com>
2023-01-18 22:55:58 +0200

    right editor changes

--- src/js/pages/Editor/cmps/accordion.jsx
+++ src/js/pages/Editor/cmps/accordion.jsx
@@ -1,44 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Accordion from '@radix-ui/react-accordion'
 import classNames from 'classnames'
 import { ChevronDownIcon } from '@radix-ui/react-icons'
+import { useSelector } from 'react-redux'
+import { saveCmp } from '../../../store/wap/wap.action'
+import SelectUnit from './ui-cmps/select'
 
-const AccordionDemo = () => (
-    <Accordion.Root className='AccordionRoot' type='single' defaultValue='item-1' collapsible>
-        <Accordion.Item className='AccordionItem' value='item-1'>
-            <AccordionTrigger>Size</AccordionTrigger>
-            <AccordionContent></AccordionContent>
-        </Accordion.Item>
+const AccordionEdit = () => {
+    const [openOption, setOpenOption] = useState()
+    const lastClickedElem = useSelector(storeState => storeState.wapModule.clickedElem)
 
-        <Accordion.Item className='AccordionItem' value='item-2'>
-            <AccordionTrigger>Position</AccordionTrigger>
-            <AccordionContent>
-                Yes. It's unstyled by default, giving you freedom over the look and feel.
-            </AccordionContent>
-        </Accordion.Item>
+    const sizeOptions = [
+        { name: 'width', title: 'width', unit: 'px' },
+        { name: 'height', title: 'height', unit: 'px' },
+        { name: 'minWidth', title: 'min-W', unit: 'px' },
+        { name: 'minHeight', title: 'min-h', unit: 'px' },
+        { name: 'maxWidth', title: 'max-w', unit: 'px' },
+        { name: 'maxHeight', title: 'max-h', unit: 'px' },
+    ]
+    function handleChange(ev) {
+        ev.preventDefault()
+        if (!lastClickedElem) return
+        const { name, value } = ev.target
+        const unit = ev.target.getAttribute('info')
+        if (lastClickedElem.style) {
+            lastClickedElem.style = { ...lastClickedElem.style, [name]: `${value + unit}` }
+        } else {
+            lastClickedElem.style = { [name]: `${value + unit}` }
+        }
+        saveCmp(lastClickedElem)
+    }
 
-        <Accordion.Item className='AccordionItem' value='item-3'>
-            <AccordionTrigger>Adjust</AccordionTrigger>
-            <Accordion.Content className='AccordionContent'>
-                <div className='AccordionContentText'>Yes! You can animate the Accordion with CSS or JavaScript.</div>
-            </Accordion.Content>
-        </Accordion.Item>
-    </Accordion.Root>
-)
+    const AccordionTrigger = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
+        <Accordion.Header className='AccordionHeader'>
+            <Accordion.Trigger className={classNames('AccordionTrigger', className)} {...props} ref={forwardedRef}>
+                {children}
+                <ChevronDownIcon className='AccordionChevron' aria-hidden />
+            </Accordion.Trigger>
+        </Accordion.Header>
+    ))
 
-const AccordionTrigger = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Header className='AccordionHeader'>
-        <Accordion.Trigger className={classNames('AccordionTrigger', className)} {...props} ref={forwardedRef}>
-            {children}
-            <ChevronDownIcon className='AccordionChevron' aria-hidden />
-        </Accordion.Trigger>
-    </Accordion.Header>
-))
+    const AccordionContent = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
+        <Accordion.Content className={classNames('AccordionContent', className)} {...props} ref={forwardedRef}>
+            <div className='AccordionContentText'>{children}</div>
+        </Accordion.Content>
+    ))
 
-const AccordionContent = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
-    <Accordion.Content className={classNames('AccordionContent', className)} {...props} ref={forwardedRef}>
-        <div className='AccordionContentText'>{children}</div>
-    </Accordion.Content>
-))
+    return (
+        <Accordion.Root className='AccordionRoot' type='single' defaultValue='item-1' collapsible>
+            <Accordion.Item className='AccordionItem' value='item-1'>
+                <AccordionTrigger>Size</AccordionTrigger>
+                <AccordionContent>
+                    <div className='option-body'>
+                        {sizeOptions.map((option, idx) => (
+                            <div key={idx} className='param-box'>
+                                <label htmlFor={option.name}>{option.title}</label>
+                                <div className='input-wrapper'>
+                                    <input
+                                        info={option.unit}
+                                        type='number'
+                                        name={option.name}
+                                        id={option.name}
+                                        onChange={handleChange}
+                                    />
+                                    <div className='unit'>
+                                        <SelectUnit />
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </AccordionContent>
+            </Accordion.Item>
 
-export default AccordionDemo
+            <Accordion.Item className='AccordionItem' value='item-2'>
+                <AccordionTrigger>Position</AccordionTrigger>
+                <AccordionContent>
+                    <SelectUnit />
+                </AccordionContent>
+            </Accordion.Item>
+
+            <Accordion.Item className='AccordionItem' value='item-3'>
+                <AccordionTrigger>Adjust</AccordionTrigger>
+                <Accordion.Content className='AccordionContent'>
+                    <div className='AccordionContentText'>
+                        Yes! You can animate the Accordion with CSS or JavaScript.
+                    </div>
+                </Accordion.Content>
+            </Accordion.Item>
+        </Accordion.Root>
+    )
+}
+
+export default AccordionEdit
