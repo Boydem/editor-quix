e831be49fb8e82fb0c04d9256183c2392ace6b47
Harel Sultan <harel714@gmail.com>
2023-01-18 21:22:24 +0200

    Supporting styling cmps with dynamic units

--- src/js/pages/Editor/cmps/edit-modules.jsx
+++ src/js/pages/Editor/cmps/edit-modules.jsx
@@ -3,6 +3,7 @@ import { AiOutlineClose } from 'react-icons/ai'
 import { RiArrowDropRightLine } from 'react-icons/ri'
 import { RiArrowDropDownLine } from 'react-icons/ri'
 import { useSelector } from 'react-redux'
+import { saveCmp } from '../../../store/wap/wap.action'
 import AccordionDemo from './accordion'
 
 export function EditModules({ setSidebarOpen, setActiveModule, activeModule }) {
@@ -12,20 +13,23 @@ export function EditModules({ setSidebarOpen, setActiveModule, activeModule }) {
     const sizeOptions = [
         { name: 'width', title: 'width', unit: 'px' },
         { name: 'height', title: 'height', unit: 'px' },
-        { name: 'min-width', title: 'min-W', unit: 'px' },
-        { name: 'min-height', title: 'min-h', unit: 'px' },
-        { name: 'max-width', title: 'max-w', unit: 'px' },
-        { name: 'max-height', title: 'max-h', unit: 'px' },
+        { name: 'minWidth', title: 'min-W', unit: 'px' },
+        { name: 'minHeight', title: 'min-h', unit: 'px' },
+        { name: 'maxWidth', title: 'max-w', unit: 'px' },
+        { name: 'maxHeight', title: 'max-h', unit: 'px' },
     ]
 
     function handleChange(ev) {
         ev.preventDefault()
+        if (!lastClickedElem) return
         const { name, value } = ev.target
+        const unit = ev.target.getAttribute('info')
         if (lastClickedElem.style) {
-            lastClickedElem.style = { ...lastClickedElem.style, [name]: `${value}px` }
+            lastClickedElem.style = { ...lastClickedElem.style, [name]: `${value + unit}` }
         } else {
-            lastClickedElem.style = { [name]: `${value}px` }
+            lastClickedElem.style = { [name]: `${value + unit}` }
         }
+        saveCmp(lastClickedElem)
     }
 
     return (
@@ -57,7 +61,13 @@ export function EditModules({ setSidebarOpen, setActiveModule, activeModule }) {
                             {sizeOptions.map((option, idx) => (
                                 <div key={idx} className='param-box'>
                                     <label htmlFor={option.name}>{option.title}</label>
-                                    <input type='number' name={option.name} id={option.name} onChange={handleChange} />
+                                    <input
+                                        info={option.unit}
+                                        type='number'
+                                        name={option.name}
+                                        id={option.name}
+                                        onChange={handleChange}
+                                    />
                                 </div>
                             ))}
                         </div>
@@ -73,7 +83,13 @@ export function EditModules({ setSidebarOpen, setActiveModule, activeModule }) {
                             {sizeOptions.map((option, idx) => (
                                 <div key={idx} className='param-box'>
                                     <label htmlFor={option.name}>{option.title}</label>
-                                    <input type='number' name={option.name} id={option.name} onChange={handleChange} />
+                                    <input
+                                        info={option.unit}
+                                        type='number'
+                                        name={option.name}
+                                        id={option.name}
+                                        onChange={handleChange}
+                                    />
                                 </div>
                             ))}
                         </div>
