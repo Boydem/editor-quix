ccb0175aecd1fb796a7b475b47b6affd244be2bc
Boydem <noyemdahan@gmail.com>
2023-01-18 20:28:26 +0200

    accordion

--- src/js/pages/Editor/cmps/accordion.jsx
+++ src/js/pages/Editor/cmps/accordion.jsx
@@ -6,19 +6,19 @@ import { ChevronDownIcon } from '@radix-ui/react-icons'
 const AccordionDemo = () => (
     <Accordion.Root className='AccordionRoot' type='single' defaultValue='item-1' collapsible>
         <Accordion.Item className='AccordionItem' value='item-1'>
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-            <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+            <AccordionTrigger>Size</AccordionTrigger>
+            <AccordionContent></AccordionContent>
         </Accordion.Item>
 
         <Accordion.Item className='AccordionItem' value='item-2'>
-            <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+            <AccordionTrigger>Position</AccordionTrigger>
             <AccordionContent>
                 Yes. It's unstyled by default, giving you freedom over the look and feel.
             </AccordionContent>
         </Accordion.Item>
 
         <Accordion.Item className='AccordionItem' value='item-3'>
-            <AccordionTrigger>Can it be animated?</AccordionTrigger>
+            <AccordionTrigger>Adjust</AccordionTrigger>
             <Accordion.Content className='AccordionContent'>
                 <div className='AccordionContentText'>Yes! You can animate the Accordion with CSS or JavaScript.</div>
             </Accordion.Content>
