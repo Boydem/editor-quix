464565868b97c72dc3edf3170325ac8b34526867
ForeseN <yagosik@gmail.com>
2023-01-19 20:41:10 +0200

    added support for many inputs in forms

--- src/js/pages/Editor/cmps/dynamic-cmps/form-cmp.jsx
+++ src/js/pages/Editor/cmps/dynamic-cmps/form-cmp.jsx
@@ -8,18 +8,26 @@ import { PCmp } from './p-cmp'
 import TextCmp from './txt-cmp'
 
 export function FormCmp({ cmp, handleClick, onHover }) {
-    const [inputValue, setInputValue] = useState('')
+    const inputsMap = cmp.cmps.reduce((acc, c) => {
+        if (c.type !== 'input') return acc
+
+        acc[c.name] = ''
+        return acc
+    }, {})
+    const [inputsValues, setInputsValues] = useState(inputsMap)
 
     function onSubmit(ev) {
         ev.preventDefault()
-        console.log(inputValue)
+        for (const key of Object.keys(inputsValues)) {
+            console.log(`The value for ${key} is: ${inputsValues[key]}`)
+        }
     }
 
     function handleChange(ev) {
         const value = ev.target.value
         const field = ev.target.name
 
-        setInputValue(value)
+        setInputsValues(prev => ({ ...prev, [field]: value }))
     }
 
     return (
@@ -32,12 +40,27 @@ export function FormCmp({ cmp, handleClick, onHover }) {
             onSubmit={onSubmit}
         >
             {cmp.cmps?.map(innerCmp => {
-                if (innerCmp.type === 'input') {
+                if (innerCmp.type === 'label') {
+                    return (
+                        <label
+                            className={innerCmp.name}
+                            key={innerCmp.id}
+                            style={cmp.style}
+                            onClick={e => handleClick(e, cmp)}
+                            placeholder={cmp.content?.placeholder}
+                            // name={innerCmp.name}
+                            onMouseOver={onHover}
+                            onMouseOut={ev => ev.currentTarget.classList.remove('hover')}
+                            onChange={handleChange}
+                        ></label>
+                    )
+                } else if (innerCmp.type === 'input') {
                     return (
                         <input
                             className={innerCmp.name}
                             key={innerCmp.id}
                             style={cmp.style}
+                            name={innerCmp.name}
                             onClick={e => handleClick(e, cmp)}
                             placeholder={cmp.content?.placeholder}
                             onMouseOver={onHover}
