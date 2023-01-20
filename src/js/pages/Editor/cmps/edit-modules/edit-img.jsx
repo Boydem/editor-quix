167fe3a3bdad8175ec94e70d3767519d1e3ceb7d
ForeseN <yagosik@gmail.com>
2023-01-20 15:34:04 +0200

    added image context

--- src/js/pages/Editor/cmps/edit-modules/edit-img.jsx
+++ src/js/pages/Editor/cmps/edit-modules/edit-img.jsx
@@ -1,3 +1,20 @@
+import { useRef } from 'react'
+import { BsChevronDown } from 'react-icons/bs'
+
 export function EditImg() {
-    return <div>EditImg</div>
+    const expandedRef = useRef()
+    function setIsExpanded() {
+        expandedRef.current.classList.toggle('hidden')
+    }
+
+    return (
+        <div className='adjust inside-accordion'>
+            <div className='header' onClick={setIsExpanded}>
+                <p>Image</p>
+                <button>
+                    <BsChevronDown />
+                </button>
+            </div>
+        </div>
+    )
 }
