4aa49b3e0568b3c8ad1bd3d56fb62e0eab783056
Harel Sultan <harel714@gmail.com>
2023-01-20 21:26:48 +0200

    merged conflict

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -5,11 +5,8 @@ import { Home } from './js/pages/Home/home'
 import { WapIndex } from './js/pages/Template/wap-index'
 import { Editor } from './js/pages/Editor/editor'
 import { Preview } from './js/pages/Preview/preview'
-<<<<<<< HEAD
 import { LoginSignup } from './js/pages/Login/login-signup'
-=======
 import { Dashboard } from './js/pages/Dashboard/dashboard'
->>>>>>> b140120372798ba31b07d466a0e6b021c8c36ccb
 
 function App() {
     return (
