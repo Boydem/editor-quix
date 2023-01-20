6305621cede4c4e3c046ed3ba42c91e2c961c5a3
ForeseN <yagosik@gmail.com>
2023-01-20 20:48:46 +0200

    adding map edit

--- src/js/pages/Editor/cmps/edit-modules/edit-map.jsx
+++ src/js/pages/Editor/cmps/edit-modules/edit-map.jsx
@@ -8,12 +8,26 @@ export function EditMap({ clickedCmp }) {
         lng: clickedCmp.content.lng,
         zoom: clickedCmp.content.zoom,
     })
+    const markers = clickedCmp.content?.markers.reduce((acc, marker) => {
+        acc.push({ lat: marker.lat, lng: marker.lng, id: +marker.id })
+        return acc
+    }, [])
+    console.log(markers)
     function setIsExpanded() {
         expandedRef.current.classList.toggle('hidden')
     }
     const expandedRef = useRef()
 
-    function handleChange(ev) {
+    function handleMapChange(ev) {
+        const value = ev.target.value
+        const field = ev.target.name
+
+        setLatLng(prev => ({ ...prev, [field]: value }))
+        console.log('clickedCmp.content[field]', clickedCmp.content[field])
+        clickedCmp.content[field] = parseFloat(value)
+        saveCmp(clickedCmp)
+    }
+    function handleMarkerChange(ev) {
         const value = ev.target.value
         const field = ev.target.name
 
@@ -33,20 +47,38 @@ export function EditMap({ clickedCmp }) {
                 </button>
             </div>
 
-            <div className='expanded-content hidden' ref={expandedRef}>
+            <div className='expanded-content hidden edit-map' ref={expandedRef}>
                 <h6>Map</h6>
                 <label htmlFor=''>
                     Lat
-                    <input type='text' name='lat' value={latLng.lat} onChange={handleChange} />
+                    <input type='text' name='lat' value={latLng.lat} onChange={handleMapChange} />
                 </label>
                 <label htmlFor=''>
                     Lng
-                    <input type='text' name='lng' value={latLng.lng} onChange={handleChange} />
+                    <input type='text' name='lng' value={latLng.lng} onChange={handleMapChange} />
                 </label>
                 <label htmlFor=''>
                     Zoom
-                    <input type='text' name='zoom' value={latLng.zoom} onChange={handleChange} />
+                    <input type='text' name='zoom' value={latLng.zoom} onChange={handleMapChange} />
                 </label>
+
+                <h6>Markers</h6>
+                {markers.map(marker => {
+                    return (
+                        <div className='marker' key={marker.id}>
+                            <label htmlFor=''>
+                                Lat
+                                <input type='text' name='zoom' value={marker.lat} onChange={handleMarkerChange} />
+                            </label>
+                            <label htmlFor=''>
+                                Lng
+                                <input type='text' name='zoom' value={marker.lng} onChange={handleMarkerChange} />
+                            </label>
+                            <button>X</button>
+                        </div>
+                    )
+                })}
+                <button className='btn'>Add Marker</button>
             </div>
         </div>
     )
