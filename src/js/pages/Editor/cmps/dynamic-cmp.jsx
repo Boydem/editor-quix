686ab4825be1910cc13350829994b57209fe9bf2
ForeseN <yagosik@gmail.com>
2023-01-20 02:10:27 +0200

    fixed serious bug with event in dynamic elem

--- src/js/pages/Editor/cmps/dynamic-cmp.jsx
+++ src/js/pages/Editor/cmps/dynamic-cmp.jsx
@@ -11,6 +11,7 @@ import { VideoCmp } from './dynamic-cmps/video-cmp'
 export default function DynamicCmp(props) {
     const elClickedNode = useSelector(storeState => storeState.wapModule.elClickedNode)
     const isEditing = useSelector(storeState => storeState.wapModule.isEditing)
+    // const selectedActionsRef = props.selectedActionsRef
 
     function handleClick(ev, cmp) {
         ev.stopPropagation()
@@ -22,6 +23,10 @@ export default function DynamicCmp(props) {
             elClickedNode.classList.remove('clicked')
         }
         ev.target.classList.add('clicked')
+        // console.log(props.selectedActionsRef.current)
+        // console.log(ev.target.offsetTop)
+        // props.selectedActionsRef.current.style.top = `${ev.target.offsetTop - 10 + 135}px`
+        // props.selectedActionsRef.current.style.left = `${ev.target.offsetLeft + 10 + 85}px`
 
         setElClickedNode(ev.target)
         setClickedCmp(cmp)
@@ -35,7 +40,7 @@ export default function DynamicCmp(props) {
 
         ev.currentTarget.classList.add('hover')
     }
-    const basicProps = { cmp: props.cmp, handleClick, onHover }
+    const basicProps = { cmp: props.cmp, handleClick, onHover, selectedActionsRef: props.selectedActionsRef }
     switch (props.cmp.type) {
         case 'form':
             return <FormCmp {...basicProps} />
@@ -45,8 +50,6 @@ export default function DynamicCmp(props) {
             return <MapCmp {...basicProps} />
         case 'video':
             return <VideoCmp {...basicProps} />
-        // case 'a':
-        //     return <ACmp {...basicProps} />
         case 'img':
             return <ImgCmp {...basicProps} />
         default:
