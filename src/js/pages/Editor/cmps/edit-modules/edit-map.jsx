fe0432e5b6dd94650262bcc2140f801f68fcd9a2
ForeseN <yagosik@gmail.com>
2023-01-20 21:51:21 +0200

    fixed bugs with map

--- src/js/pages/Editor/cmps/edit-modules/edit-map.jsx
+++ src/js/pages/Editor/cmps/edit-modules/edit-map.jsx
@@ -3,6 +3,7 @@ import { BsChevronDown } from 'react-icons/bs'
 import { locationService } from '../../../../services/location.service'
 import { makeId } from '../../../../services/util.service'
 import { saveCmp } from '../../../../store/wap/wap.action'
+import { FaTrash } from 'react-icons/fa'
 
 export function EditMap({ clickedCmp }) {
     const [geoLocationValue, setGeoLocationValue] = useState('')
@@ -20,21 +21,23 @@ export function EditMap({ clickedCmp }) {
     //     lng: clickedCmp.content?.lng,
     //     zoom: clickedCmp.content?.zoom,
     // })
+    const [zoom, setZoom] = useState(clickedCmp.content?.zoom)
     // const markers = clickedCmp.content?.markers?.reduce((acc, marker) => {
     //     acc.push({ lat: marker.lat, lng: marker.lng, id: +marker.id })
     //     return acc
     // }, [])
     // console.log(markers)
 
-    // function handleMapChange(ev) {
-    //     const value = ev.target.value
-    //     const field = ev.target.name
+    function handleMapChange(ev) {
+        const value = ev.target.value
+        const field = ev.target.name
 
-    //     setLatLng(prev => ({ ...prev, [field]: value }))
-    //     console.log('clickedCmp.content[field]', clickedCmp.content[field])
-    //     clickedCmp.content[field] = parseFloat(value)
-    //     saveCmp(clickedCmp)
-    // }
+        setZoom(+value)
+        // console.log('clickedCmp.content[field]', clickedCmp.content[field])
+        clickedCmp.content[field] = parseFloat(value)
+        saveCmp(clickedCmp)
+    }
+
     // function handleMarkerChange(ev) {
     //     const value = ev.target.value
     //     const field = ev.target.name
@@ -93,17 +96,18 @@ export function EditMap({ clickedCmp }) {
                 <label htmlFor=''>
                     Lng
                     <input type='text' name='lng' value={latLng.lng} onChange={handleMapChange} />
-                </label>
+                </label> */}
                 <label htmlFor=''>
                     Zoom
-                    <input type='text' name='zoom' value={latLng.zoom} onChange={handleMapChange} />
+                    <input type='text' name='zoom' value={zoom} onChange={handleMapChange} />
                 </label>
- */}
+
                 <h6>Markers</h6>
-                {clickedCmp.content.markers.map(marker => {
-                    return (
-                        <div className='marker' key={marker.id}>
-                            {/* <label htmlFor=''>
+                <div className='markers'>
+                    {clickedCmp.content.markers.map(marker => {
+                        return (
+                            <div className='marker' key={marker.id}>
+                                {/* <label htmlFor=''>
                                 Lat
                                 <input type='text' name='zoom' value={marker.lat} onChange={handleMarkerChange} />
                             </label>
@@ -111,12 +115,15 @@ export function EditMap({ clickedCmp }) {
                                 Lng
                                 <input type='text' name='zoom' value={marker.lng} onChange={handleMarkerChange} />
                             </label> */}
-                            <p>{marker.name}</p>
-                            <button onClick={() => onDeleteMarker(marker)}>X</button>
-                        </div>
-                    )
-                })}
-                <button className='btn'>Add Marker</button>
+                                <button className='btn' onClick={() => onDeleteMarker(marker)}>
+                                    <FaTrash />
+                                </button>
+                                <p>{marker.name}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+                {/* <button className='btn'>Add Marker</button> */}
             </div>
         </div>
     )
