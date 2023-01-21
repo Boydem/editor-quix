43eba38be25e17683b1a038780d6b89d5f875897
ForeseN <yagosik@gmail.com>
2023-01-21 22:49:43 +0200

    added right sidebar empty state

---
+++ src/js/pages/Editor/cmps/edit-modules/empty-right-sidebar.jsx
@@ -0,0 +1,11 @@
+export function EmptyRightSidebar() {
+    return (
+        <div className='empty-right-sidebar'>
+            <img
+                src='https://res.cloudinary.com/yaronshapira-com/image/upload/v1674333825/12123123-removebg-preview_rxm84w.png'
+                alt=''
+            />
+            <p>Select an element to see and edit its properties here.</p>
+        </div>
+    )
+}
--- src/js/pages/Editor/cmps/right-sidebar.jsx
+++ src/js/pages/Editor/cmps/right-sidebar.jsx
@@ -1,32 +1,37 @@
 import { AiOutlineClose } from 'react-icons/ai'
+import { useSelector } from 'react-redux'
 import { Accordion } from './edit-accordion'
+import { EmptyRightSidebar } from './edit-modules/empty-right-sidebar'
 import { EditTabs } from './edit-tabs'
 
 export function RightSidebar({ rightSidebarState, handleSidebarsChanges }) {
+    const elClickedNode = useSelector(storeState => storeState.wapModule.elClickedNode)
     function handleSidebar(stateChanges) {
         handleSidebarsChanges('right', stateChanges)
     }
 
     return (
         <div className={`right-sidebar ${rightSidebarState.isOpen ? 'open' : ''}`}>
-            <div className='module-content'>
-                <div className='module-header'>
-                    <span className='module-name'>{rightSidebarState.context}</span>
-                    <div className='actions'>
-                        <span
-                            onClick={() => {
-                                handleSidebar({ isOpen: !rightSidebarState.isOpen })
-                            }}
-                            className='btn'
-                        >
-                            <AiOutlineClose />
-                        </span>
+            {!elClickedNode && <EmptyRightSidebar />}
+
+            {elClickedNode && (
+                <div className='module-content'>
+                    <div className='module-header'>
+                        <span className='module-name'>{rightSidebarState.context}</span>
+                        <div className='actions'>
+                            <span
+                                onClick={() => {
+                                    handleSidebar({ isOpen: !rightSidebarState.isOpen })
+                                }}
+                                className='btn'
+                            >
+                                <AiOutlineClose />
+                            </span>
+                        </div>
                     </div>
+                    <EditTabs context={rightSidebarState.context} />
                 </div>
-                {/* <AccordionEdit /> */}
-                {/* <Accordion context={rightSidebarState.context} /> */}
-                <EditTabs context={rightSidebarState.context} />
-            </div>
+            )}
         </div>
     )
 }
