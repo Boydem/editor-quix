e46a8455a6f7e95bbb5f7bb80d8318e0f6642ceb
Boydem <noyemdahan@gmail.com>
2023-01-22 13:49:34 +0200

    demo1 ui refactors

--- src/js/pages/Template/cmps/dark-header.jsx
+++ src/js/pages/Template/cmps/dark-header.jsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export function DarkHeader() {
-    const [isMenuOpen, setIsMenuOpen] = useState()
-
-    function toggleMenu() {
-        setIsMenuOpen(!isMenuOpen)
-    }
     return (
         <header className='dark-header app-header full'>
             <div className='logo-container'>
@@ -14,35 +8,6 @@ export function DarkHeader() {
                     Webix.
                 </Link>
             </div>
-            <nav className={`main-nav ${isMenuOpen ? 'open' : ''}`}>
-                <ul className='flex align-center'>
-                    <li>
-                        <a className='nav-link link-underline' href='#'>
-                            <span>Site</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a className='nav-link link-underline' href='#'>
-                            <span>Add</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a className='nav-link link-underline' href='#'>
-                            <span>View</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a className='nav-link link-underline' href='#'>
-                            <span>Tools</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a className='nav-link link-underline' href='#'>
-                            <span>Help</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
         </header>
     )
 }
