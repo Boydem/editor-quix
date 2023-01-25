69b8334b5117bb3a812ce5b67145e81fdfa143b4
ForeseN <yagosik@gmail.com>
2023-01-25 22:40:21 +0200

    added dnd

--- src/js/pages/Dashboard/cmps/kanban-dashboard.jsx
+++ src/js/pages/Dashboard/cmps/kanban-dashboard.jsx
@@ -5,6 +5,7 @@ import { userService } from '../../../services/user.service'
 import { makeId } from '../../../services/util.service'
 import { SET_USER } from '../../../store/user/user.reducer'
 import { FiTrash } from 'react-icons/fi'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 export function KanbanDashboard({ user, currSite }) {
     const [newItemText, setNewItemText] = useState({})
@@ -36,37 +37,63 @@ export function KanbanDashboard({ user, currSite }) {
         setBoards([...boards])
         // dispatch({ type: SET_USER, user })
     }
+    let totalIndex
     console.log('boards:', boards)
     if (!boards || !boards.length) return <div>Loading..</div>
     return (
-        <section className='kanban-board info-box'>
-            {user?.boards &&
-                boards.map((board, idx) => (
-                    <div key={board._id} className='list-wrapper'>
-                        <div className='list-content list'>
-                            <div className='list-header'>{board.title}</div>
-                            <ul className='list-items'>
-                                {board.items?.map(item => (
-                                    <li key={item._id}>
-                                        <button className='btn-icon close' onClick={onDelete}>
-                                            <FiTrash />
-                                        </button>
-                                        <span>{item.txt}</span>
-                                    </li>
+        <DragDropContext>
+            <section className='kanban-board info-box'>
+                <Droppable droppableId='kanban-board'>
+                    {provided => {
+                        return (
+                            <div {...provided.droppableProps} ref={provided.innerRef}>
+                                {boards.map((board, idx) => (
+                                    <div key={board._id} className='list-wrapper'>
+                                        <div className='list-content list'>
+                                            <div className='list-header'>{board.title}</div>
+                                            <ul className='list-items'>
+                                                {board.items?.map((item, index) => {
+                                                    return (
+                                                        <Draggable key={item._id} draggableId={item._id} index={index}>
+                                                            {provided => {
+                                                                return (
+                                                                    <li
+                                                                        {...provided.draggableProps}
+                                                                        {...provided.dragHandleProps}
+                                                                        ref={provided.innerRef}
+                                                                    >
+                                                                        <button
+                                                                            className='btn-icon close'
+                                                                            onClick={onDelete}
+                                                                        >
+                                                                            <FiTrash />
+                                                                        </button>
+                                                                        <span>{item.txt}</span>
+                                                                    </li>
+                                                                )
+                                                            }}
+                                                        </Draggable>
+                                                    )
+                                                })}
+                                            </ul>
+                                            <form onSubmit={ev => addNewItem(ev, board._id, idx)} className='add-item'>
+                                                <input
+                                                    onChange={handleChange}
+                                                    value={newItemText[board._id] || ''}
+                                                    type='text'
+                                                    name={board._id}
+                                                    placeholder='Add a card'
+                                                />
+                                            </form>
+                                        </div>
+                                    </div>
                                 ))}
-                            </ul>
-                            <form onSubmit={ev => addNewItem(ev, board._id, idx)} className='add-item'>
-                                <input
-                                    onChange={handleChange}
-                                    value={newItemText[board._id] || ''}
-                                    type='text'
-                                    name={board._id}
-                                    placeholder='Add a card'
-                                />
-                            </form>
-                        </div>
-                    </div>
-                ))}
-        </section>
+                                {provided.placeholder}
+                            </div>
+                        )
+                    }}
+                </Droppable>
+            </section>
+        </DragDropContext>
     )
 }
