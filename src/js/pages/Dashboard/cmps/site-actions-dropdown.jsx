5dc0d73ac67d7176ce54040c71093bbb16e5b135
Boydem <noyemdahan@gmail.com>
2023-01-26 21:33:55 +0200

    awdawd

--- src/js/pages/Dashboard/cmps/site-actions-dropdown.jsx
+++ src/js/pages/Dashboard/cmps/site-actions-dropdown.jsx
@@ -21,76 +21,17 @@ export function SiteActionsDropdown() {
             </DropdownMenu.Trigger>
 
             <DropdownMenu.Portal>
-                <DropdownMenu.Content className='DropdownMenuContent' sideOffset={5}>
-                    <DropdownMenu.Item className='DropdownMenuItem'>
-                        New Tab <div className='RightSlot'>⌘+T</div>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item className='DropdownMenuItem'>
-                        New Window <div className='RightSlot'>⌘+N</div>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item className='DropdownMenuItem' disabled>
-                        New Private Window <div className='RightSlot'>⇧+⌘+N</div>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Sub>
-                        <DropdownMenu.SubTrigger className='DropdownMenuSubTrigger'>
-                            More Tools
-                            <div className='RightSlot'>
-                                <ChevronRightIcon />
-                            </div>
-                        </DropdownMenu.SubTrigger>
-                        <DropdownMenu.Portal>
-                            <DropdownMenu.SubContent className='DropdownMenuSubContent' sideOffset={2} alignOffset={-5}>
-                                <DropdownMenu.Item className='DropdownMenuItem'>
-                                    Save Page As… <div className='RightSlot'>⌘+S</div>
-                                </DropdownMenu.Item>
-                                <DropdownMenu.Item className='DropdownMenuItem'>Create Shortcut…</DropdownMenu.Item>
-                                <DropdownMenu.Item className='DropdownMenuItem'>Name Window…</DropdownMenu.Item>
-                                <DropdownMenu.Separator className='DropdownMenu.Separator' />
-                                <DropdownMenu.Item className='DropdownMenuItem'>Developer Tools</DropdownMenu.Item>
-                            </DropdownMenu.SubContent>
-                        </DropdownMenu.Portal>
-                    </DropdownMenu.Sub>
-
+                <DropdownMenu.Content className='DropdownMenuContent' sideOffset={5} align={'end'}>
+                    <DropdownMenu.Item className='DropdownMenuItem'>Preview</DropdownMenu.Item>
+                    <DropdownMenu.Item className='DropdownMenuItem'>Publish</DropdownMenu.Item>
                     <DropdownMenu.Separator className='DropdownMenuSeparator' />
-
-                    <DropdownMenu.CheckboxItem
-                        className='DropdownMenuCheckboxItem'
-                        checked={bookmarksChecked}
-                        onCheckedChange={setBookmarksChecked}
-                    >
-                        <DropdownMenu.ItemIndicator className='DropdownMenuItemIndicator'>
-                            <CheckIcon />
-                        </DropdownMenu.ItemIndicator>
-                        Show Bookmarks <div className='RightSlot'>⌘+B</div>
-                    </DropdownMenu.CheckboxItem>
-                    <DropdownMenu.CheckboxItem
-                        className='DropdownMenuCheckboxItem'
-                        checked={urlsChecked}
-                        onCheckedChange={setUrlsChecked}
-                    >
-                        <DropdownMenu.ItemIndicator className='DropdownMenuItemIndicator'>
-                            <CheckIcon />
-                        </DropdownMenu.ItemIndicator>
-                        Show Full URLs
-                    </DropdownMenu.CheckboxItem>
-
+                    <DropdownMenu.Item className='DropdownMenuItem'>Invite People to Collaborate</DropdownMenu.Item>
                     <DropdownMenu.Separator className='DropdownMenuSeparator' />
-
-                    <DropdownMenu.Label className='DropdownMenuLabel'>People</DropdownMenu.Label>
-                    <DropdownMenu.RadioGroup value={person} onValueChange={setPerson}>
-                        <DropdownMenu.RadioItem className='DropdownMenuRadioItem' value='pedro'>
-                            <DropdownMenu.ItemIndicator className='DropdownMenuItemIndicator'>
-                                <DotFilledIcon />
-                            </DropdownMenu.ItemIndicator>
-                            Pedro Duarte
-                        </DropdownMenu.RadioItem>
-                        <DropdownMenu.RadioItem className='DropdownMenuRadioItem' value='colm'>
-                            <DropdownMenu.ItemIndicator className='DropdownMenuItemIndicator'>
-                                <DotFilledIcon />
-                            </DropdownMenu.ItemIndicator>
-                            Colm Tuite
-                        </DropdownMenu.RadioItem>
-                    </DropdownMenu.RadioGroup>
+                    <DropdownMenu.Item className='DropdownMenuItem'>Rename Site</DropdownMenu.Item>
+                    <DropdownMenu.Item className='DropdownMenuItem'>Create New Site</DropdownMenu.Item>
+                    <DropdownMenu.Item className='DropdownMenuItem'>Duplicate Site</DropdownMenu.Item>
+                    <DropdownMenu.Separator className='DropdownMenuSeparator' />
+                    <DropdownMenu.Item className='DropdownMenuItem'>Dashboard</DropdownMenu.Item>
 
                     <DropdownMenu.Arrow className='DropdownMenuArrow' />
                 </DropdownMenu.Content>
