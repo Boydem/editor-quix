8a43a8ad98f95707724cc751d7fe198f8b30e8d8
Boydem <noyemdahan@gmail.com>
2023-01-27 19:33:15 +0200

    schedule component designed

--- src/js/wap-templates/general-cmps/general-cmps.js
+++ src/js/wap-templates/general-cmps/general-cmps.js
@@ -3,7 +3,8 @@ import mapAndForm from './map-and-form.json'
 import chat2 from './chat2.json'
 import { social1 } from './social-1'
 import schedule from './schedule.json'
+import schedule2 from './schedule-2.json'
 
 export function getDemoGeneralCmps() {
-    return [form1, mapAndForm, chat2, social1, schedule]
+    return [form1, mapAndForm, chat2, social1, schedule, schedule2]
 }
