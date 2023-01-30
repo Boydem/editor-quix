d150d216c39835f609c695cd96a1c53885e79a53
ForeseN <yagosik@gmail.com>
2023-01-30 14:17:57 +0200

    cleaned editor code

--- src/js/pages/Editor/editor.jsx
+++ src/js/pages/Editor/editor.jsx
@@ -23,10 +23,8 @@ export function Editor() {
     const clickedCmp = useSelector(storeState => storeState.wapModule.clickedCmp)
     const [layout, setLayout] = useState({ layoutClass: 'desktopLayout', width: '' })
     const cursorRef = useRef()
-    const cursorIntervalIdRef = useRef()
     const { wapId } = useParams()
     const dispatch = useDispatch()
-    let debounceMousePosCheckRef = useRef()
     let mousePosTimeOutIdRef = useRef()
 
     function debounceRemoveMouseCursor() {
@@ -51,11 +49,6 @@ export function Editor() {
     useEffect(() => {
         loadWap()
         setIsEditing(true)
-        // cursorIntervalIdRef.current = setInterval(() => {
-        //     if (cursorRef.current) {
-        //         cursorRef.current.style.display = 'none'
-        //     }
-        // }, 3500)
         socketService.emit('set-wap-room', wapId)
         socketService.off('updated-wap')
         socketService.on('updated-wap', wap => {
@@ -74,18 +67,20 @@ export function Editor() {
             socketService.emit('update-mouse-pos', { mouseX: ev.clientX, mouseY: ev.clientY })
         }
 
-        // setInterval(() => {
-        //     console.log('cursorRef:', cursorRef)
-        // }, 1000)
-
         return () => {
             socketService.off('mouse-move')
             socketService.off('updated-wap')
             setIsEditing(false)
             setWapNull()
-            // clearInterval(cursorIntervalIdRef.current)
             setElClickedNode(null)
             document.removeEventListener('mousemove', emitMouseMovement)
+
+            if (mousePosTimeOutIdRef.current) {
+                clearTimeout(mousePosTimeOutIdRef.current)
+            }
+            if (cursorRef.current) {
+                cursorRef.current.style.display = 'none'
+            }
         }
     }, [])
 
